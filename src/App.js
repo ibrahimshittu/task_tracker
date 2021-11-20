a6cdbcb05614c7c9ba22e161de34275983cff6f3
Ibrahim Shittu <ibshittu01@gmail.com>
2021-11-20 11:33:06 +0100

    added task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Header from './components/Header';
-import Task from './components/Task';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
+
+  const [tasks, setTask] = useState([
+    {   id: 1,  title: 'Task 1', description: 'Task 1 description', status: 'open'},
+    {   id: 2,  title: 'Task 2', description: 'Task 2 description', status: 'open'},
+    {   id: 3,  title: 'Task 3', description: 'Task 3 description', status: 'open'},
+    {   id: 4,  title: 'Task 4', description: 'Task 4 description', status: 'open'},
+    {   id: 5,  title: 'Task 5', description: 'Task 5 description', status: 'open'},]);
+
+
+  const deleteTask = (id) => {
+    console.log('deleted', id)
+  }
   return (
     <div className="container">
       <Header />
-      <Task/>
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,18 @@
-import {useState} from 'react';
+import {FaTimes} from 'react-icons/fa';
 
 
-
-const Task = () => {
-
-    const [task, setTask] = useState([
-    {   id: 1,  title: 'Task 1', description: 'Task 1 description', status: 'open'},
-    {   id: 2,  title: 'Task 2', description: 'Task 2 description', status: 'open'},
-    {   id: 3,  title: 'Task 3', description: 'Task 3 description', status: 'open'},
-    {   id: 4,  title: 'Task 4', description: 'Task 4 description', status: 'open'},
-    {   id: 5,  title: 'Task 5', description: 'Task 5 description', status: 'open'},]);
+const Task = ({ task, onDelete }) => {
     return (
-        <>
-            {task.map((task) => (<h3 key={task.id}>{task.title}</h3>))}
-        </>
+        <div className="task">
+            <h3>{task.title} <FaTimes style={ faTimesStyle } onClick={()=>onDelete(task.id) } /></h3>
+            <p>{task.description}</p>
+        </div>
     )
+}
+
+const faTimesStyle = {
+    color: 'red',
+    cursor: 'pointer',
 }
 
 export default Task
