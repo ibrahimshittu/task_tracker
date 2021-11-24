18dacab0bfd42b5b8d1d57e3d131e9ae028077d9
Ibrahim Shittu <ibshittu01@gmail.com>
2021-11-24 02:05:41 +0100

    button onClick toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,12 @@ import PropTypes from 'prop-types'
 import Button from './Button'	
 import React from 'react'
 
-const Header = ({title}) => {
-
-    const onClick = () => {
-        console.log('Clicked')
-    }
+const Header = ({title, onAdd }) => {
 
     return (
         <header className="header">
             <h1 style = {{}}>{title}</h1>
-            <Button color = "green" text="Add" onclick={onClick}/>
+            <Button color = "green" text="Add" onClick={onAdd}/>
         </header>
     )
 }
